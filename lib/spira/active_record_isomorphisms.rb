29215d82c5e18e543beb3cb08fd2f99cf1f1cff3
Nicholas Hurden <git@nhurden.com>
2014-12-28 15:01:07 +1100

    Extract symbol manipulations.

--- lib/spira/active_record_isomorphisms.rb
+++ lib/spira/active_record_isomorphisms.rb
@@ -14,15 +14,15 @@ module Spira
 
     module ClassMethods
       def isomorphic_with(ar_name)
-        raise NoDefaultVocabularyError, 'A default vocabulary must be set' unless default_vocabulary
+        raise NoDefaultVocabularyError, 'A default vocabulary must be set.' unless default_vocabulary
 
         # Define the foreign key property
-        id = ar_name.to_s + '_id'
-        if properties.keys.include? id
+        id_sym = id_getter(ar_name)
+        if properties.keys.include? id_sym.to_s
           raise IsomorphismAlreadyDefinedError,
-            "An isomorphism with #{ar_name} has already been established or the property #{id} is already in use."
+            "An isomorphism with #{ar_name} has already been established or the property #{id_sym} is already in use."
         end
-        property id.to_sym, type: Spira::Types::Integer
+        property id_sym, type: Spira::Types::Integer
 
         define_active_record_methods(ar_name)
         define_spira_methods(ar_name)
@@ -30,12 +30,28 @@ module Spira
 
       private
 
+      def append_to_symbol(symbol, suffix)
+        (symbol.to_s + suffix).to_sym
+      end
+
+      def setter(name)
+        append_to_symbol(name, '=')
+      end
+
+      def id_getter(name)
+        append_to_symbol(name, '_id')
+      end
+
+      def id_setter(name)
+        append_to_symbol(name, '_id=')
+      end
+
       def define_active_record_methods(ar_name)
         ar_class = model_class_for_sym(ar_name)
         spira_class = self
         spira_name = spira_class.name.underscore.to_sym
 
-        # Get the Spira model from the ActiveRecord model
+        # Get the Spira model from the ActiveRecord model by querying the repository
         ar_class.send(:define_method, spira_name) do
           predicate = RDF::Vocabulary.new(spira_class.default_vocabulary)["/#{ar_name}_id"]
           object = RDF::Literal.new(id, datatype: RDF::XSD.integer)
@@ -43,15 +59,11 @@ module Spira
           model_iri ? spira_class.for(model_iri) : nil
         end
 
-        # Set the Spira model on the ActiveRecord model
-        ar_class.send(:define_method, (spira_name.to_s + '=').to_sym) do |new_model|
+        # Set the Spira model on the ActiveRecord model by updating ids on the Spira models
+        id_setter_name = id_setter(ar_name)
+        ar_class.send(:define_method, setter(spira_name)) do |new_model|
           old_model = self.send(spira_name)
-          id_setter_name = (ar_name.to_s + '_id=').to_sym
-
-          # Remove id on the old Spira model
           old_model.send(id_setter_name, nil) if old_model
-
-          # Set id on the new Spira model
           new_model.send(id_setter_name, self.id) if new_model
         end
       end
@@ -60,15 +72,16 @@ module Spira
         ar_class = model_class_for_sym(ar_name)
 
         # Get the ActiveRecord model from the Spira model
+        id_getter_name = id_getter(ar_name)
         define_method(ar_name) do
-          id = self.send((ar_name.to_s + '_id').to_sym)
+          id = self.send(id_getter_name)
           ar_class.find(id)
         end
 
         # Set the ActiveRecord model on the Spira model
-        define_method((ar_name.to_s + '=').to_sym) do |new_model|
-          method_name = (ar_name.to_s + '_id=').to_sym
-          self.send(method_name, new_model.try(:id))
+        id_setter_name = id_setter(ar_name)
+        define_method(setter(ar_name)) do |new_model|
+          self.send(id_setter_name, new_model.try(:id))
         end
       end
 
